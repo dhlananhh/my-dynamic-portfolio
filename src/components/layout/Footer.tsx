ad198559a54cba7e197e7d4256964521b3b0393a
Lan Anh <dhlananh2309@gmail.com>
2025-07-08 12:10:44 +0700

    feat: update footer text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -109,7 +109,7 @@ export default function Footer() {
         <div className="mt-8 pt-8 border-t border-gray-200">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-md">
-              © { new Date().getFullYear() } Lan Anh. All rights reserved.
+              Designed & Built by Lan Anh. © { new Date().getFullYear() } All rights reserved.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               <Link
