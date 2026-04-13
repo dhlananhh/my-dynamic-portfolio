2743013018cf426eff4a1073f2a49859b32e49d9
dhlananhh <dhlananh2309@gmail.com>
2026-04-13 15:19:48 +0700

    chore: update code

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,21 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config();
-
-// kiểm tra biến môi trường đã load chưa
-console.log("MONGODB_URI:", process.env.MONGODB_URI);
-
+import path from "path";
+import sharp from "sharp";
 import { buildConfig } from "payload";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
-import path from "path";
-import sharp from "sharp"
-import { fileURLToPath } from "url";
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
 import { Projects } from "./collections/Projects";
 import { Certifications } from "./collections/Certificates";
 import { Testimonials } from "./collections/Testimonials";
 import { WorkExperiences } from "./collections/WorkExperiences";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") })
+
+// kiểm tra biến môi trường đã load chưa
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 
 export default buildConfig({
@@ -33,12 +32,11 @@ export default buildConfig({
   editor: lexicalEditor({}),
   secret: process.env.PAYLOAD_SECRET || "strongp@ssword123",
   typescript: {
-    // outputFile: path.resolve(dirname, "../src/payload-types.ts"),
-    outputFile: path.resolve(process.cwd(), "src/payload-types.ts"),
+    outputFile: path.resolve(__dirname, "./payload-types.ts"),
     declare: false,
   },
   graphQL: {
-    schemaOutputFile: path.resolve(process.cwd(), "src/payload-generated-schema.ts"),
+    schemaOutputFile: path.resolve(__dirname, "./payload-generated-schema.ts"),
   },
   db: mongooseAdapter({
     url: process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio",
@@ -46,10 +44,12 @@ export default buildConfig({
       serverSelectionTimeoutMS: 5000,
       bufferCommands: false,
       family: 4
-    }
+    },
   }),
   sharp,
   plugins: [
 
   ],
 });
+
+// module.exports = config;
